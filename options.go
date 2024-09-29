c8a071847541b393ef81fb2fb42d4c30682b456c
Doodl0 <52604696+Doodl0@users.noreply.github.com.>
2024-09-29 21:18:18 +0100

    Fixed program not continuing if no input was entered for an option

--- options.go
+++ options.go
@@ -18,7 +18,7 @@ func AskUserOptions() {
 	fmt.Print(CopyFilesWithoutExtensions.description)
 	// Get user's input
 	var input string
-	fmt.Scan(&input)
+	fmt.Scanln(&input)
 	// Change option based on user input
 	if input == "y" || input == "Y" {
 		CopyFilesWithoutExtensions.variable = true
@@ -29,7 +29,7 @@ func AskUserOptions() {
 	// Ask user the option
 	fmt.Print(DeleteFilesAfterCopying.description)
 	// Get user's input
-	fmt.Scan(&input)
+	fmt.Scanln(&input)
 	// Change option based on user input
 	if input == "y" || input == "Y" {
 		DeleteFilesAfterCopying.variable = true
